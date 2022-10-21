44a57ee480502ea09fc4fefafe5265da58103ce3
thinh hoang <hoang.thinh.dhc@gmail.com>
2022-10-21 09:57:04 +0700

    Initial commit

---
+++ lib/views/my_home_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../controllers/my_home_page_controller.dart';
+import 'about_page.dart';
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  final MyHomePageController controller = Get.put(MyHomePageController());
+  MyHomePage({required this.title});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Obx(
+                  () => Text(
+                '${controller.count.value}',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Get.off(AboutPage());
+                },
+                child: Text('About GetX')),
+            ElevatedButton(
+                onPressed: () {
+                  Get.snackbar('GetX Snackbar', 'Yay! Awesome GetX Snackbar',
+                      snackPosition: SnackPosition.BOTTOM,
+                      backgroundColor: Colors.amberAccent);
+                },
+                child: Text('Show Snackbar')),
+            ElevatedButton(
+                onPressed: () {
+                  Get.defaultDialog(
+                      title: 'GetX Alert',
+                      middleText: 'Simple GetX alert',
+                      textConfirm: 'Okay',
+                      confirmTextColor: Colors.amberAccent,
+                      textCancel: 'Cancel');
+                },
+                child: Text('Show AlertDialog'))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: controller.increment,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
